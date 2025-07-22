2a8c30eec26c81a9e06c07cf1c6679e596e4db4b
Eddy <emont017@fiu.edu>
2025-07-22 15:34:22 -0400

    updated text on index.php

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/../includes/header.php'; // This file starts the sessio
 <section id="hero">
     <h1>Hotel Management System</h1>
     <p class="subtitle">
-        This system was developed as part of our Senior Capstone Project for Florida International University’s College of Engineering & Computing. It provides core functionalities for administrators to manage room inventory, monitor bookings, and support day-to-day operations, inspired by enterprise-level tools such as Hilton’s OnQ platform.
+        This system was developed as part of our Senior Capstone Project for Florida International University’s Knight Foundation School of Computing and Information Sciences. It provides core functionalities for administrators to manage room inventory, monitor bookings, and support day-to-day operations, inspired by enterprise-level tools such as Hilton’s OnQ platform.
 		<h4>Development Team: Carmine Talarico, Jonathan Gonzalez, Edward Montes, Alberto Enrique Santalo Jr</h4>
     </p>
 
